63889b097b07f9cd856ce003954e09205d13594e
Carlos Bentin <cbentin12@gmail.com>
2022-06-25 11:13:49 -0500

    Adding gitignore and deleting old files

--- src/io/keyboard.cpp
+++
@@ -1,9 +0,0 @@
-#include "keyboard.hpp"
-
-Keyboard::Keyboard() {
-    this->velocity = 0.0f;
-    this->angle = 0.0f;
-    this->printData = false;
-    this->acceleration = 0;
-    this->called = false;
-}
--- src/physics/collision.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef COLLISION_HPP
-#define COLLISION_HPP
-
-#include "../entities/player.hpp"
-#include "../worlds/map.hpp"
-
-vec2 map_collision(vec2 pos, vec2 newPos, vec2 velocity, Map *map);
-
-#endif
